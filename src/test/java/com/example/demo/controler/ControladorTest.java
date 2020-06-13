f123e25d0348d6972ec1b500204008907025025a
PabloD97 <pablo97758@gmail.com>
2020-06-12 21:17:22 -0300

    Agregando los test de aceptacion

--- src/test/java/com/example/demo/controler/ControladorTest.java
+++ src/test/java/com/example/demo/controler/ControladorTest.java
@@ -1,49 +1,57 @@
 package com.example.demo.controler;
 
-
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.demo.interfaces.ILibro;
 import com.example.demo.modelo.Libro;
-import com.example.demo.service.LibroService;
-
-
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-class ControladorTest {
-
-
+@DataJpaTest
+public class ControladorTest {
+	
+	
 	@Autowired
-	private LibroService service;
+	private ILibro libroService;
 	
-	@BeforeEach
-	void setUp() throws Exception {
-		service = new LibroService();
-		
+	@Test
+	public void comoAdministradorDeseoVerTodosLosLibros() {
+	    // given
+		Libro libro1 = new Libro(1, "libro1", "libron", 0, "librito", 0);
+		Libro libro2 = new Libro(2, "libro2", "libron", 0, "librito", 0);
+		Libro libro3 = new Libro(3, "libro3", "libron", 0, "librito", 0);
+		  
+	    // when
+	    
+		libroService.save(libro1);
+		libroService.save(libro2);
+		libroService.save(libro3);
+	    
+		List<Libro> listaDeLibro = (List<Libro>) libroService.findAll();
+	    
+	    // then
+	    assertTrue( !listaDeLibro.isEmpty() );
 	}
 
 	@Test
-	void test() {
-		Libro libro1 = new Libro(4,"LibroTest","Nacho",5,"GeneroTest",8);
-		service.save(libro1);
-		
-		assertTrue(service.listarTodos().size() > 0);
+	public void comoAdministradorDeseoAgregarUnLibro() {
+	    // given
+		Libro libro1 = new Libro(0, "libro1", "libron", 0, "librito", 0);
+	 
+	    // when
+	    
+		Libro libroGuardado = libroService.save(libro1);
+	    List<Libro> listaDeLibro = (List<Libro>) libroService.findAll();
+	    
+	    // then
+	    assertTrue( listaDeLibro.contains(libroGuardado) );
 	}
-	
-	@Test
-	void test2() {
-		assertTrue(true);
-	}
-
 }
+
